047e278e74874d5e7be63569611b66e48acaaa5f
Evan Dorsky <evan.dorsky@icloud.com>
2017-12-04 23:36:55 -0800

    Date stateholding

--- disp.sh
+++ disp.sh
@@ -2,20 +2,24 @@
 
 function date_fix {
 	key_input=""
+	date_format="%Y-%m-%d %H:%m"
+	date_format_date="%Y-%m-%d"
+	current_date=$(date "+$date_format")
 	for filename in $1/*.jpg; do
 		echo $filename
-		date
 		while :
 		do
 			read -rsn1 key_input
 			case $key_input in
 				j)
-					echo date goes down
+					current_date=$(date -jv -1d -f "$date_format" "$current_date" "+$date_format")
+					echo $current_date
 					;;
 				k)
-					echo date goes up
+					current_date=$(date -jv +1d -f "$date_format" "$current_date" "+$date_format")
+					echo $current_date
 					;;
-				p)
+				g)
 					echo done
 					break
 					;;
